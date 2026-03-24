bc866743610b55818785987b12e46a44d79bfb9e
Jkker <j@erry.dev>
2026-03-24 11:38:59 -0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite-plus'
+
+export default defineConfig({
+  staged: {
+    '*': 'vp check --fix',
+  },
+  pack: {
+    dts: {
+      tsgo: true,
+    },
+    exports: true,
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {
+    semi: false,
+    singleQuote: true,
+    experimentalSortPackageJson: true,
+    experimentalSortImports: {
+      partitionByComment: true,
+    },
+  },
+})
